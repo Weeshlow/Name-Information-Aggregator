e7f52ff7431f3c51c0870f701c3512d2737a6361
chackerian <nchackerian@gmail.com>
2016-06-02 13:19:22 -0400

    LOTS

--- client/main.js
+++ client/main.js
@@ -1,36 +1,36 @@
 "use strict";
 
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import angularMeteor from 'angular-meteor';
 import ngMessages from 'angular-messages';
-import { Accounts } from 'meteor/accounts-base';
-// import { SearchHist } from '../collections/db.js'
+import {Accounts} from 'meteor/accounts-base';
+// export { firstName, lastName}
 
-	Accounts.ui.config({
-		passwordSignupFields: 'USERNAME_ONLY',
-	});
-	// import phantomjsWrapper from 'phantomjs-wrapper';
+Accounts.ui.config({
+	passwordSignupFields: 'USERNAME_ONLY',
+});
+// import phantomjsWrapper from 'phantomjs-wrapper';
 
-	angular.module('info', [
-		angularMeteor, 'ngMaterial', 'ngMessages', 'accounts.ui'
-	])
-		.controller('SearchCtrl', SearchCtrl)
-		.directive('savedSearch', searchy)
+angular.module('info', [
+	angularMeteor, 'ngMaterial', 'ngMessages', 'accounts.ui'
+])
+	.controller('SearchCtrl', SearchCtrl)
+	.directive('savedSearch', searchy)
 
-	function onReady() {
-		angular.bootstrap(document, ['info']);
-	}
+function onReady() {
+	angular.bootstrap(document, ['info']);
+}
 
-	// Saved Directive
-	function searchy() {
-		return {
-			restrict: 'E',
-			templateUrl: 'client/templates/partials/saved.html',
-			controller: 'SearchCtrl'
-		};
+// Saved Directive
+function searchy() {
+	return {
+		restrict: 'E',
+		templateUrl: 'client/templates/partials/saved.html',
+		controller: 'SearchCtrl'
+	};
 
-	}
-	const SearchHist = new Mongo.Collection('searches');
+}
+const SearchHist = new Mongo.Collection('searches');
 
 	// Main Controller
 	function SearchCtrl($timeout, $q, $scope, $rootScope) {
@@ -59,25 +59,26 @@ import { Accounts } from 'meteor/accounts-base';
 
 
 			var name = querys.split(" ");
-			var firstName = name[0];
-			var lastName = name[1];
+			const firstName = name[0];
+			const lastName = name[1];
 
 			// URLS
 			var ancestryURL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fwww.ancestry.com%2Fname-origin%3Fsurname%3D" + lastName + "'%20and%20xpath%3D'%2F%2F*%5B%40id%3D%22topContainer%22%5D%2Fsection%5B1%5D%2Fdiv%2Fdl%5B1%5D%2Fdd'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-			var piplURL = "https://pipl.com/search/?q="+ firstName + "+" + lastName + "&l=&sloc=&in=5"
-			var spokeoURL = "http://www.spokeo.com/" + firstName + "-" + lastName;
+			var spokeoURL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fwww.spokeo.com%2F"+ firstName + "-" + lastName + "%20and%20xpath%3D'%2F%2Fa%5B%40class%3D%22listview_section%22%5D'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
 			var whitepagesURL = "http://www.whitepages.com/name/" + firstName + "-" + lastName;
-			var inteliusURL  = "http://www.intelius.com/search/people/" + firstName + "-" + lastName;
-			var nameURL = "https://namechk.com/"
+			// var nameURL = "https://namechk.com/";
 
 			$scope.result = {};
 
-			$.getJSON(ancestryURL, function(result){
+			$.getJSON(ancestryURL, function(result) {
 				var ancestry = result.query.results.dd;
-				$(".ancestry").add("<h3>" + ancestry + "</h3>");
+				console.log(ancestry);
+				$(".ancestry").replaceWith("<h3 class='ancestry'>" + ancestry + "</h3>");
 			});
 
-			//*[@id="match_results"
+			Meteor.call('phantom', firstName, lastName, function(err, result) {
+				console.log(result);
+			})
 
 			$scope.hidestatus = false;
 			$scope.master.searches.push(querys);
@@ -103,16 +104,16 @@ import { Accounts } from 'meteor/accounts-base';
 
 		}
 
-		$scope.removeDB = function(){
+		$scope.removeDB = function() {
 			Meteor.call('dbEmpty');
 			$scope.searches = "";
 		}
 
 		$scope.scrollInfo = function() {
 
-		$("body").animate({
-			scrollTop: $("#info").offset().top
-		}, 1000);
+			$("body").animate({
+				scrollTop: $("#info").offset().top
+			}, 1000);
 
 		}
 
@@ -135,11 +136,10 @@ import { Accounts } from 'meteor/accounts-base';
 		$(".search").keypress(function(event) {
 			var key = event.which;
 
-			if (key == 13){
+			if (key == 13) {
 				$(".search-button").click();
 			}
 		});
-
 
 		$scope.title1 = 'Button';
 		$scope.title4 = 'Warn';
@@ -149,7 +149,7 @@ import { Accounts } from 'meteor/accounts-base';
 
 	}
 
-	function querySearch(query) {
-		var results = query ? self.states.filter(createFilterFor(query)) : [];
-		return results;
-	}
+function querySearch(query) {
+	var results = query ? self.states.filter(createFilterFor(query)) : [];
+	return results;
+}
